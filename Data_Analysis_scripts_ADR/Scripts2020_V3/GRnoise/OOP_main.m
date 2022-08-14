1e4ab4647e764b1df77eb2b9f4b69fa0fddd9527
Sietse de Boer <sietsedeboer22@hotmail.com>
2022-08-14 03:46:03 +0200

    now have 1 scatter plot that is quite intersting. Power less..

--- Data_Analysis_scripts_ADR/Scripts2020_V3/GRnoise/OOP_main.m
+++ Data_Analysis_scripts_ADR/Scripts2020_V3/GRnoise/OOP_main.m
@@ -3,6 +3,7 @@
 close all
 
 %% (1) Single plots
+figurepath = ['..' filesep '..' filesep '..' filesep 'Export_Figures_noGit\OOP\automatisch' filesep ]
 FFTsubsubdir=['Data_LT254_Sietse' filesep 'LT254_Sietse_Chip11' filesep 'Noise_vs_T' filesep 'FFT' filesep '2D'];% This is where the
 addpath('..\subroutines')
 %% Plotting: single plot!
@@ -60,6 +61,14 @@ legend('Data','TLS fit','GR + sys noise','Combined')
 
 
 %% Fknee compare G3,G4
+P_iter = 7; 
+T_iter = 1:14;
+%-------
+SW.plotdata = 0;
+SW.plottls = 0;
+SW.plotgr = 0;
+SW.plotFknee = 1;
+%-------
 fignum=2;
 axnum = fignum;
 Tcolors = colormapJetJB(14);
@@ -79,6 +88,49 @@ o.plotsingle(fignum,axnum,kidn,Pindex,Tindex,SW,'o',Tcolors(Tindex,:))
 end
 end
 end
+filename = 'G3G4compare';
+saveas(gca,[figurepath  filename '.fig' ])
+exportgraphics(gca,[figurepath  filename '.png' ]) 
+exportgraphics(gca,[figurepath filename '.pdf' ]) 
+
+
+
+
+
+
+%% Fknee compare G3,G4
+P_iter = 1:7; 
+T_iter = 3;
+%-------
+SW.plotdata = 0;
+SW.plottls = 0;
+SW.plotgr = 0;
+SW.plotFknee = 1;
+%-------
+fignum=2;
+axnum = fignum;
+Pcolors = colormapcoolSdB(7);
+o = o.init_figax(fignum,axnum,'loglin');
+for kidn = 1:3 % G3
+for Pindex= P_iter
+for Tindex= T_iter
+o.plotsingle(fignum,axnum,kidn,Pindex,Tindex,SW,'x',Pcolors(Pindex,:))
+end
+end
+end
+
+for kidn = 4:6 %G4
+for Pindex= P_iter
+for Tindex= T_iter
+o.plotsingle(fignum,axnum,kidn,Pindex,Tindex,SW,'o',Pcolors(Pindex,:))
+end
+end
+end
+filename = 'G3G4compareP';
+saveas(gca,[figurepath  filename '.fig' ])
+exportgraphics(gca,[figurepath  filename '.png' ]) 
+exportgraphics(gca,[figurepath filename '.pdf' ]) 
+
 
 
 
